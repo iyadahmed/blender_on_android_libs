952b37edffbefba0823f9f03fb826f534a28b4a1
Iyad Ahmed <iyadahmed430@gmail.com>
2023-02-28 11:18:44 +0200

    More progress on unified build script

--- download_and_build_deps.py
+++ download_and_build_deps.py
@@ -1,6 +1,11 @@
 import os
-from pathlib import Path
+import platform
 import tarfile
+from dataclasses import dataclass
+from pathlib import Path
+import shutil
+import subprocess as sp
+import multiprocessing as mp
 
 import requests
 from tqdm import tqdm
@@ -11,14 +16,35 @@ SOURCES_DOWNLOAD_PATH = Path(".") / "sources"
 if not SOURCES_DOWNLOAD_PATH.exists():
     os.mkdir(SOURCES_DOWNLOAD_PATH)
 
+LIB_BUILD_OUTPUT_PATH = (Path(".") / "build/lib/android_aarch64").absolute()
+if not LIB_BUILD_OUTPUT_PATH.exists():
+    os.makedirs(LIB_BUILD_OUTPUT_PATH)
 
-def download_pkg_source(pkg_filename: str) -> Path:
-    url = f"{BLENDER_SVN_PKGS_ROOT}/{pkg_filename}"
-    save_path = SOURCES_DOWNLOAD_PATH / pkg_filename
 
-    with requests.get(url, stream=True) as download_stream, open(
-        save_path, "wb"
-    ) as file:
+def find_android_ndk():
+    home = Path.home()
+    system_name = platform.system()
+
+    if system_name == "Linux":
+        return home / "Android/Sdk/ndk/25.1.8937393"
+
+    elif system_name == "Windows":
+        return home / "AppData/Local/Android/Sdk/ndk/25.1.8937393"
+
+    elif system_name == "Darwin":
+        return home / "Library/Android/sdk/ndk/25.1.8937393"
+
+    else:
+        raise NotImplementedError
+
+
+NDK = os.getenv("ANDROID_NDK", find_android_ndk())
+API = 21
+ABI = "arm64-v8a"
+
+
+def download_file(url: str, save_path: Path) -> Path:
+    with requests.get(url, stream=True) as download_stream, open(save_path, "wb") as file:
         download_stream.raise_for_status()
 
         # Progress bar for file downloads: https://stackoverflow.com/a/42071418/8094047
@@ -33,10 +59,54 @@ def download_pkg_source(pkg_filename: str) -> Path:
     return save_path
 
 
-download_pkg_source("brotli-v1.0.9.tar.gz")
-download_pkg_source("libepoxy-1.5.10.tar.gz")
-download_pkg_source("freetype-2.12.1.tar.gz")
-download_pkg_source("libjpeg-turbo-2.1.3.tar.gz")
-download_pkg_source("libpng-1.6.37.tar.xz")
-download_pkg_source("Python-3.10.9.tar.xz")
-download_pkg_source("zstd-1.5.0.tar.gz")
+def download_pkg_source(pkg_filename: str):
+    save_path = SOURCES_DOWNLOAD_PATH / pkg_filename
+
+    if save_path.exists():
+        print(f"Skipping download, {pkg_filename} already downloaded.")
+        return save_path
+
+    url = f"{BLENDER_SVN_PKGS_ROOT}/{pkg_filename}"
+    return download_file(url, save_path)
+
+
+def build_pkg_cmake(pkg_name: str, pkg_filename: str):
+    filepath = download_pkg_source(pkg_filename)
+    with tarfile.open(filepath) as file:
+        top_dir = os.path.commonpath(file.getnames())
+        assert top_dir != "."
+        file.extractall(SOURCES_DOWNLOAD_PATH)
+
+    sp.run(
+        [
+            "cmake",
+            "-S",
+            ".",
+            "-B",
+            "_build",
+            "-DCMAKE_BUILD_TYPE=Release",
+            f"-DCMAKE_INSTALL_PREFIX={LIB_BUILD_OUTPUT_PATH / pkg_name}",
+            "-DCMAKE_SYSTEM_NAME=Android",
+            f"-DCMAKE_SYSTEM_VERSION={API}",
+            f"-DCMAKE_ANDROID_ARCH_ABI={ABI}",
+            f"-DCMAKE_ANDROID_NDK={NDK}",
+            "-DCMAKE_ANDROID_STL_TYPE=c++_static",
+        ],
+        cwd=SOURCES_DOWNLOAD_PATH / top_dir,
+    )
+    sp.run(
+        ["cmake", "--build", ".", "--config", "Release", "-j", f"{mp.cpu_count()}"],
+        cwd=SOURCES_DOWNLOAD_PATH / top_dir / "_build",
+    )
+    sp.run(["cmake", "--install", "."], cwd=SOURCES_DOWNLOAD_PATH / top_dir / "_build")
+
+
+if __name__ == "__main__":
+    build_pkg_cmake("brotli", "brotli-v1.0.9.tar.gz")
+
+    download_pkg_source("libepoxy-1.5.10.tar.gz")
+    download_pkg_source("freetype-2.12.1.tar.gz")
+    download_pkg_source("libjpeg-turbo-2.1.3.tar.gz")
+    download_pkg_source("libpng-1.6.37.tar.xz")
+    download_pkg_source("Python-3.10.9.tar.xz")
+    download_pkg_source("zstd-1.5.0.tar.gz")
